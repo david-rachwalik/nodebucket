4d2f92ca91e3596d311a8b22f1236e1ae88c17b8
David Rachwalik <david.rachwalik@outlook.com>
2022-08-24 00:45:00 -0500

    updated client model with todo items

---
+++ src/app/shared/models/item.interface.ts
@@ -0,0 +1,13 @@
+/*
+============================================
+; Title:        item.interface.ts
+; Author:       David Rachwalik
+; Date:         2022/08/22
+; Description:  Interface for Employee task documents
+;===========================================
+*/
+
+export interface Item {
+  _id: string;
+  text: string;
+}
