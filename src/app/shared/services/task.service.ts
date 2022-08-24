e347ccec1bb138980ce66b70486c7477fa651f4b
David Rachwalik <david.rachwalik@outlook.com>
2022-08-24 00:45:30 -0500

    added task item service to client

---
+++ src/app/shared/services/task.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+/*
+============================================
+; Title:        task.service.ts
+; Author:       David Rachwalik
+; Date:         2022/08/22
+; Description:  Service for employee tasks
+;===========================================
+*/
+
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from '../models/employee.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  constructor(private http: HttpClient) {}
+
+  /**
+   * findAllTasks
+   */
+  // findAllTasks(empId: number): Observable<any> {
+  findAllTasks(empId: number): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`/api/employees/${empId}/tasks`);
+  }
+
+  /**
+   * createTask
+   */
+  // createTask(empId: number, task: string): Observable<any> {
+  createTask(empId: number, task: string): Observable<Employee[]> {
+    return this.http.post<Employee[]>(`/api/employees/${empId}/tasks`, {
+      text: task,
+    });
+  }
+}
