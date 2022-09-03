81c44a589e00826f2de752ec1bacad71f1f774d5
David Rachwalik <david.rachwalik@outlook.com>
2022-09-03 00:49:22 -0500

    stricter SessionService type checking

--- src/app/shared/services/session.service.ts
+++ src/app/shared/services/session.service.ts
@@ -11,13 +11,16 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+import { BaseResponse } from '../models/base-response.interface';
+import { Employee } from '../models/employee.interface';
+
 @Injectable({
   providedIn: 'root',
 })
 export class SessionService {
   constructor(private http: HttpClient) {}
 
-  findEmployeeById(empId: number): Observable<any> {
-    return this.http.get(`/api/employees/${empId}`);
+  findEmployeeById(empId: number): Observable<BaseResponse<Employee>> {
+    return this.http.get<BaseResponse<Employee>>(`/api/employees/${empId}`);
   }
 }
